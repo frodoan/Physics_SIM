5c5488bf1864735e71f9dd20744455c3e374a179
frodoan <45771950+frodoan@users.noreply.github.com>
2018-12-19 22:23:38 +0000

    intial commit
    intial GPS

---
+++ Physics_Sim/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics_Sim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string version1 = "1";
+            string name = "Gideon Physics Sim";
+
+            Console.WriteLine("version " + version1);
+            Console.WriteLine(name);
+
+
+            //isrunning tells us that the program is running.
+            while (isrunning)
+            {
+                Console.WriteLine("please enter a command");
+                string CMD = Console.ReadLine();
+                Command_Process(CMD);
+            }
+        }
+
+        static bool isrunning = true;
+
+        static void Command_Process(string CMD)
+        {
+            if (CMD == "Hello World")
+            {
+                isrunning = false;
+
+
+
+            }
+
+
+        }
+    }
+}
